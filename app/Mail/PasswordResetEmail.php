0fa4990660ab206b59ccb261b5be8858a7c9c8af
IvaneMatiashvili <ivanematiashvili@gmail.com>
2023-01-10 02:42:55 +0400

    feat: create password reset end verify email mails

---
+++ app/Mail/PasswordResetEmail.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class PasswordResetEmail extends Mailable
+{
+	use Queueable, SerializesModels;
+
+	public $email;
+
+	public $url;
+
+	/**
+	 * Create a new message instance.
+	 *
+	 * @return void
+	 */
+	public function __construct($email, $url)
+	{
+		$this->email = $email;
+		$this->url = $url;
+	}
+
+	/**
+	 * Get the message envelope.
+	 *
+	 * @return \Illuminate\Mail\Mailables\Envelope
+	 */
+	public function envelope()
+	{
+		return new Envelope(
+			subject: 'Password Reset Email',
+		);
+	}
+
+	/**
+	 * Get the message content definition.
+	 *
+	 * @return \Illuminate\Mail\Mailables\Content
+	 */
+	public function content()
+	{
+		return new Content(
+			view: 'emails.password-reset-email',
+		);
+	}
+
+	/**
+	 * Get the attachments for the message.
+	 *
+	 * @return array
+	 */
+	public function attachments()
+	{
+		return [];
+	}
+}
