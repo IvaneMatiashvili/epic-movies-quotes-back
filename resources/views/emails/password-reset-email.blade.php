0655653301b07b5791532e5bf242803f7829c20a
IvaneMatiashvili <ivanematiashvili@gmail.com>
2023-01-10 02:35:53 +0400

    feat: create a markap for email message

---
+++ resources/views/emails/password-reset-email.blade.php
@@ -0,0 +1,120 @@
+<x-email-layout>
+    <div class="main" style="
+        height: 95vh;
+        overflow: hidden;
+     background: linear-gradient(187.16deg, #181623 0.07%, #191725 51.65%, #0D0B14 98.75%);
+    ">
+        <img src="{{ $message->embed(public_path().'/assets/icon.png') }}" alt="landing image" style="display: block;
+        margin-top: 5rem;
+        margin-left: auto;
+        margin-right: auto;
+        "
+        />
+
+        <img src="{{ $message->embed(public_path().'/assets/movie-quotes.png') }}" alt="landing image" style="display: block;
+        margin-top: 1rem;
+        margin-left: auto;
+        margin-right: auto;
+        "
+        />
+        <p class="confirmation" style="font-weight: 400;
+        font-size: 18px;
+        margin-left: 11rem;
+        margin-top: 3.5rem;
+        color: #FFFFFF;
+        font-family: 'Inter', 'sans-serif';">
+            {{ __('content.hola') }}
+        </p>
+
+        <p class="confirmation" style="font-weight: 400;
+        font-size: 18px;
+        margin-left: 11rem;
+        margin-top: 2rem;
+        color: #FFFFFF;
+        font-family: 'Inter', 'sans-serif';">
+            {{ __('content.resetPassword') }}
+        </p>
+        <a href="{{ urldecode('http://localhost:3000/?stage=passwordReset&feedback=').$url  }}" style=";
+        margin-top: 2.5rem;
+        font-size: 18px;
+        font-weight: 400;
+        color: #FFFFFF;
+        background: #E31221;
+        display: block;
+        text-align: center;
+        border: 0.8rem solid #E31221;
+        height: 1.3rem;
+        width: 10rem;
+        margin-left: 11rem;
+        border-radius: 8px;
+        text-decoration: none;
+        font-family: 'Inter', 'sans-serif';">
+            {{ __('content.resetPasswordBtn') }}
+        </a>
+
+        <p class="confirmation" style="font-weight: 400;
+        font-size: 18px;
+        margin-left: 11rem;
+        margin-top: 3rem;
+        color: #FFFFFF;
+        font-family: 'Inter', 'sans-serif';">
+            {{ __('content.clickDoesNotWork') }}
+        </p>
+
+        <p class="confirmation" style="font-weight: 400;
+        font-size: 18px;
+        margin-left: 11rem;
+        margin-top: 2rem;
+        color: #DDCCAA;
+        font-family: 'Inter', 'sans-serif';">
+            {{urldecode('http://localhost:3000/?stage=passwordReset&feedback=').$url}}
+        </p>
+
+        <p class="confirmation" style="font-weight: 400;
+        font-size: 18px;
+        margin-left: 11rem;
+        margin-top: 3rem;
+        color: #FFFFFF;
+        font-family: 'Inter', 'sans-serif';">
+            {{ __('content.anyProblems') }}
+        </p>
+
+        <p class="confirmation" style="font-weight: 400;
+        font-size: 18px;
+        margin-left: 11rem;
+        margin-top: 2.5rem;
+        color: #FFFFFF;
+        font-family: 'Inter', 'sans-serif';">
+            {{ __('content.crew') }}
+        </p>
+    </div>
+
+    <style>
+        @font-face {
+            font-family: 'Inter';
+            font-style: normal;
+        }
+        @media (max-width: 900px) {
+            body {
+                overflow: visible !important;
+            }
+            div {
+                height: 120vh !important;
+                overflow-x: hidden;
+                overflow-y: scroll;
+            }
+            p {
+                margin-left: 1rem !important;
+                word-break: break-all;
+            }
+            a {
+                margin-left: 1rem !important;
+            }
+        }
+        @media (max-width: 600px) {
+            div {
+                height: 170vh !important;
+            }
+        }
+    </style>
+</x-email-layout>
